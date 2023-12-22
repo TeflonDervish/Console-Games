446db413b0c304b3fca3e15a07d66d6282644864
Teflon Dervish <voltvilty45@gmail.com>
2023-12-22 20:08:13 +0500

    Make snake with python

--- Python/Snake/Snake.py
+++ Python/Snake/Snake.py
@@ -2,9 +2,14 @@ import os
 import numpy as np
 import time
 import keyboard as kb
+import random
 
-
-
+class GameOver(Exception):
+    def __init__(self, message="Игра окончена"):
+        self.message = message
+        super().__init__(self.message)
+        
+    
 class Game_parts:
     
     def __init__(self, x, y):
@@ -16,9 +21,26 @@ class Game_parts:
     
     def getY(self):
         return self.y
+    
+    def getType(self):
+        pass
+    
 
 class Snake_parts(Game_parts):
-    pass
+    
+    def getType(self):
+        return "snake"
+    
+
+class Fruits(Game_parts):
+    
+    def getType(self):
+        return "fruit"
+    
+    def setCoordinates(self, x, y):
+        self.x = x
+        self.y = y
+
 
 class Queue:
     
@@ -39,9 +61,14 @@ class Queue:
     def __str__(self):
         return str(self.arr)
 
+
 class Field:
     
     def __init__(self, width, height, startPosX, startPosY):
+        
+        self.score = 0
+        
+        self.fruit = Fruits(0, 0)
         
         self.snake = Queue()
         
@@ -51,10 +78,12 @@ class Field:
         self.field = np.empty((width, height), dtype=Game_parts)
                 
         self.field[startPosX, startPosY] = Snake_parts(startPosX, startPosY)
-        self.field[startPosX - 1, startPosY] = Snake_parts(startPosX - 1, startPosY)
+        self.field[startPosX + 1, startPosY] = Snake_parts(startPosX + 1, startPosY)
         
         self.snake.add(self.field[startPosX, startPosY])
-        self.snake.add(self.field[startPosX - 1, startPosY])
+        self.snake.add(self.field[startPosX + 1, startPosY])
+        
+        self.spawnFruit()
         
         
     def moveSnake(self, direction = '-1'):
@@ -62,48 +91,90 @@ class Field:
         if direction == '-1':
             direction = self.currentDirection
         
-        if direction == 'right' and self.currentDirection != 'left':
+        if direction == 'right':
+            
+            val = self.collisions((self.snake.getHead().getX() + 1) % self.width, self.snake.getHead().getY())
+            
+            if val == -1:
+                raise GameOver()
             
             self.field[(self.snake.getHead().getX() + 1) % self.width, self.snake.getHead().getY()] = \
                 Snake_parts((self.snake.getHead().getX() + 1) % self.width, self.snake.getHead().getY())
                 
             self.snake.add(self.field[(self.snake.getHead().getX() + 1) % self.width, self.snake.getHead().getY()])
             
-            snake_tail = self.snake.pop()
-            self.field[snake_tail.getX(), snake_tail.getY()] = None
         
-        elif direction == 'left' and self.currentDirection != 'right':
+                
+        elif direction == 'left':
+            
+            val = self.collisions((self.snake.getHead().getX() - 1) % self.width, self.snake.getHead().getY())
+            
+            if val == -1:
+                raise GameOver()
             
             self.field[(self.snake.getHead().getX() - 1) % self.width, self.snake.getHead().getY()] = \
                 Snake_parts((self.snake.getHead().getX() - 1) % self.width, self.snake.getHead().getY())
                 
             self.snake.add(self.field[(self.snake.getHead().getX() - 1) % self.width, self.snake.getHead().getY()])
             
-            snake_tail = self.snake.pop()
-            self.field[snake_tail.getX(), snake_tail.getY()] = None
+            
         
-        elif direction == 'down' and self.currentDirection != 'up':
+        elif direction == 'down':
+            
+            val = self.collisions(self.snake.getHead().getX(), (self.snake.getHead().getY() + 1) % self.height)
+            
+            if val == -1:
+                raise GameOver()
             
             self.field[self.snake.getHead().getX(), (self.snake.getHead().getY() + 1) % self.height] = \
                 Snake_parts(self.snake.getHead().getX(), (self.snake.getHead().getY() + 1) % self.height)
                 
             self.snake.add(self.field[self.snake.getHead().getX(), (self.snake.getHead().getY() + 1) % self.height])
             
-            snake_tail = self.snake.pop()
-            self.field[snake_tail.getX(), snake_tail.getY()] = None
+            
         
-        elif direction == 'up' and self.currentDirection != 'down':
+        elif direction == 'up':
+            
+            val = self.collisions(self.snake.getHead().getX(), (self.snake.getHead().getY() - 1) % self.height)
+            
+            if val == -1:
+                raise GameOver()
             
             self.field[self.snake.getHead().getX(), (self.snake.getHead().getY() - 1) % self.height] = \
                 Snake_parts(self.snake.getHead().getX(), (self.snake.getHead().getY() - 1) % self.height)
                 
             self.snake.add(self.field[self.snake.getHead().getX(), (self.snake.getHead().getY() - 1) % self.height])
             
+        if val == 0:
+            
             snake_tail = self.snake.pop()
             self.field[snake_tail.getX(), snake_tail.getY()] = None
+            
+        elif val == 1:
+            self.score += 1
+            self.spawnFruit() 
+                
+                
+
+    def collisions(self, x, y):
+        if self.field[x, y]:
+            if self.field[x, y].getType() == 'snake':
+                return -1
+            else:
+                return 1
+        else:
+            return 0
         
-        self.currentDirection = direction
+    def spawnFruit(self):
+        x = random.randint(0, self.width - 1)
+        y = random.randint(0, self.height - 1)
         
+        while self.collisions(x, y) == -1:
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+            
+        self.fruit.setCoordinates(x, y)
+        self.field[x, y] = self.fruit
     
     def __str__(self):
         
@@ -112,7 +183,10 @@ class Field:
             s += '|'
             for j in range(self.width):
                 if self.field[j, i]:
-                    s += '0'
+                    if self.field[j, i].getType() == 'snake':
+                        s += '0'
+                    elif self.field[j, i].getType() == 'fruit':
+                        s += '*'
                 else:
                     s += ' '
             s += '|\n'
@@ -127,7 +201,7 @@ class Field:
 
 def on_key_pressed(e):
     global direction
-    if e.event_type == kb.KEY_DOWN:
+    if e.event_type == kb.KEY_DOWN and e.name in ('left', 'right', 'up', 'down'):
         direction = e.name
             
         
@@ -144,7 +218,15 @@ if __name__ == "__main__":
     
     while True:
         kb.hook(on_key_pressed)
-        print(direction)
+        if (direction == 'left' and field.getDirection() == 'right') or \
+            (direction == 'right' and field.getDirection() == 'left') or \
+            (direction == 'up' and field.getDirection() == 'down') or \
+            (direction == 'down' and field.getDirection() == 'up'):
+                
+            direction = field.currentDirection[:]
+            
+        print(f"Scores: {field.score}")
+        field.currentDirection = direction[:]
         field.moveSnake(direction)
         print(field)
         
